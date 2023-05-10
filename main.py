38cc882852d31445b2ca08272972704ee11e5738
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2023-05-10 20:58:39 +0500

    Add commentary

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ def split_img_between_items(img: Image.Image, items: Collection) \
     width, height = img.size
     part = height // len(items)
 
+    # Not the highest value but the previous highest point on the image
     upper = 0
     for l in items:
         lower = upper + part
@@ -72,6 +73,7 @@ def split_img_between_items(img: Image.Image, items: Collection) \
 def compose_img_from_parts(parts: Iterable[Image.Image]) -> Image.Image:
     width = max(p.size[0] for p in parts)
     height = sum(p.size[1] for p in parts)
+    # Create the bigger image to fit the rest
     img = Image.new('RGB', (width, height), 'white')
 
     upper = 0
